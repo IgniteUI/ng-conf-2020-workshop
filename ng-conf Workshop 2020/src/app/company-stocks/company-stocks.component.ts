f2140f894679807d7aa31f3ea402b28fb60d1df7
Damyan Petev <damyanpetev@users.noreply.github.com>
2020-12-29 17:06:04 +0200

    fix(chart,ssr): don't render chart in SSR 🧹
    domino doesn't implement canvas API

--- ng-conf Workshop 2020/src/app/company-stocks/company-stocks.component.ts
+++ ng-conf Workshop 2020/src/app/company-stocks/company-stocks.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
+import { Component, Inject, PLATFORM_ID } from '@angular/core';
 import { CustomersService } from '../services/customers.service';
 
 @Component({
@@ -8,11 +9,13 @@ import { CustomersService } from '../services/customers.service';
 })
 export class CompanyStocksComponent {
   public chartType = 'Auto';
+  public isBrowser = true;
 
   /**
    *
    */
-  constructor(private companiesService: CustomersService) {
+  constructor(private companiesService: CustomersService, @Inject(PLATFORM_ID) platform: object) {
+    this.isBrowser = isPlatformBrowser(platform);
     this.companiesService.getCompaniesStock().subscribe(x => this.data = x);
   }
 
